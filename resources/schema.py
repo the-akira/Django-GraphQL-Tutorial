530c9588c0b97e1eadfb071a32e6642aefc75612
Gabriel Felippe <gabrielfelippe90@gmail.com>
2020-03-18 23:37:06 -0300

    pequena correcao

--- resources/schema.py
+++ resources/schema.py
@@ -70,13 +70,13 @@ class CreateArtista(graphene.Mutation):
     def mutate(root, info, input=None):
         discos = []
         for disco_input in input.discos:
-          disco = Disco.objects.get(pk=disco_input.id)
-          if disco is None:
-            return CreateArtista(disco=None)
-          discos.append(disco)
+            disco = Disco.objects.get(pk=disco_input.id)
+            if disco is None:
+                return CreateArtista(disco=None)
+            discos.append(disco)
         artista_instance = Artista(
-          nome=input.nome,
-          origem=input.origem
+            nome=input.nome,
+            origem=input.origem
           )
         artista_instance.save()
         artista_instance.discos.set(discos)
